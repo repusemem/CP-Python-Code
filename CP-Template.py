645775e851d62d922dddcde19c82b29c252b587e
Bang Lee <60475468+repusemem@users.noreply.github.com>
2025-12-17 09:38:44 +0700

    Update CP-Template.py
    improving action on local running and better I/O

--- CP-Template.py
+++ CP-Template.py
@@ -1,15 +1,8 @@
-# '{:02d}'.format(1) --> 001
-# '{0:.2f}'.format(1.5) --> 1.50
-# (a/b)%c = (a mod (b * c) / b) mod c
-# (a/b)%c = (a mod c * b^(c-2) mod c) mod c ||| c is prime number
-# To use custom compare in heapq, make a Class have a __lt__ func
-
 import sys,time,io
 #from heapq import heapify, heappush, heappop
 #from collections import defaultdict, deque, Counter, OrderedDict
 #from math import gcd,log,log2,log10
 #from bisect import bisect_left, bisect_right, insort_left, insort_right
-#from typing import List, Tuple
 #from functools import lru_cache, cmp_to_key
 
 LOCAL = ''
@@ -20,14 +13,25 @@ def IO(name = ''):
         sys.stdin = open(name+'.inp', 'r')
         sys.stdout = open(name+'.out', 'w')
 
-IO('task')
-
+if os.path.exists('task.inp'):
+    IO('task')
+    
 if LOCAL:
-    raw_input = io.BytesIO(sys.stdin.buffer.read()).readline
+    _buffer = io.BytesIO(sys.stdin.buffer.read())
+    raw_input = _buffer.readline
     def input():
-        return raw_input().decode().rstrip('\n')
+        while True:
+            line = raw_input().decode().rstrip('\n')
+            if line: return line
+            if _buffer.tell() == len(_buffer.getvalue()):
+                return ''
 else:
-    input = lambda : sys.stdin.readline().rstrip('\n')
+    _raw = sys.stdin.readline
+    def input():
+        while 1:
+            line = _raw().rstrip('\n')
+            if line: return line
+            if not line: return ''
 
 def printf(*args, sep = ' ', end = '\n'):
     sys.stdout.write(sep.join(map(str, args)) + end)
